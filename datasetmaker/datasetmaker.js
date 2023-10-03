c25b106a0ef3a1f614c2fb12e9223b6071380f0d
Nicky <raccoonutau@gmail.com>
2023-10-03 02:12:43 -0500

    Update datasetmaker.js
    removed some comments

--- datasetmaker/datasetmaker.js
+++ datasetmaker/datasetmaker.js
@@ -1,23 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 
-// Define the path to your UTAU voicebank folder and the file list
+// Define the path to your UTAU voicebank folder and the reclist
 const voicebankFolder = 'voicebank/vb';
 const fileListPath = 'reclist/reclist.txt';
 const outputFolder = 'Output';
-const numberOfCycles = 1; // Adjust this number as needed
+const numberOfCycles = 1; // Adjust the number of cycles as needed
 
 console.log(`Voicebank folder: ${voicebankFolder}`);
 console.log(`File list path: ${fileListPath}`);
 console.log(`Output folder: ${outputFolder}`);
 console.log(`Number of cycles: ${numberOfCycles}`);
 
-// Use path.resolve() to construct file paths
+// Don't worry about anything past this point
 const voicebankFolderPath = path.resolve(__dirname, voicebankFolder);
 const fileListFullPath = path.resolve(__dirname, fileListPath);
 const outputFolderPath = path.resolve(__dirname, outputFolder);
 
-// Read the file list and append '.wav' extension to filenames
 const fileContents = fs.readFileSync(fileListFullPath, 'utf-8');
 const fileArray = fileContents
   .split('\n')
@@ -25,28 +24,23 @@ const fileArray = fileContents
 
 console.log(`Read ${fileArray.length} files from the file list.`);
 
-// Get a list of subdirectories (pitch folders)
 const pitchFolders = fs.readdirSync(voicebankFolderPath)
   .filter(item => fs.statSync(path.join(voicebankFolderPath, item)).isDirectory());
 
 console.log(`Found ${pitchFolders.length} pitch folders.`);
 
-// Create the output folder if it doesn't exist
 if (!fs.existsSync(outputFolderPath)) {
   fs.mkdirSync(outputFolderPath);
   console.log(`Created output folder: ${outputFolderPath}`);
 }
 
-// Initialize counters for each pitch folder
 const pitchCounters = {};
 pitchFolders.forEach(folder => {
   pitchCounters[folder] = 0;
 });
 
-// Function to copy a file from source to destination
 function copyFile(source, destination) {
   try {
-    // Check if the destination file already exists
     let dest = destination;
     let counter = 1;
     while (fs.existsSync(dest)) {
@@ -64,14 +58,12 @@ function copyFile(source, destination) {
   }
 }
 
-// Cycle through pitch folders and copy files in a round-robin fashion
 for (let cycle = 0; cycle < numberOfCycles; cycle++) {
   for (let i = 0; i < fileArray.length; i++) {
     const pitchFolder = pitchFolders[i % pitchFolders.length];
     const sourceFile = path.resolve(voicebankFolderPath, pitchFolder, fileArray[i]);
     const destinationFile = path.resolve(outputFolderPath, fileArray[i]);
 
-    // Check if the pitch folder has reached its limit
     if (pitchCounters[pitchFolder] < Math.ceil(fileArray.length / pitchFolders.length)) {
       if (fs.existsSync(sourceFile)) {
         copyFile(sourceFile, destinationFile);
